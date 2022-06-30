df7053c605084340d1d0b05c2b9b010f6bed1a6a
Philip Lages <psilvaelages@gmail.com>
2022-06-30 18:22:15 -0300

    Try to fix lint Problem: requirement 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return species.reduce((accSpecies, currSpecies) => {
+      accSpecies[currSpecies.name] = currSpecies.residents.length;
+      return accSpecies;
+    }, {});
+  } if (!animal.sex) return species.find(({ name }) => animal.specie === name).residents.length;
+
+  return species.find(({ name }) => animal.specie === name).residents.reduce((acc, curr) => {
+    acc += (curr.sex === animal.sex) ? 1 : 0;
+    return acc;
+  }, 0);
 }
 
 module.exports = countAnimals;
