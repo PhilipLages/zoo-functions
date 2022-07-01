2478b379b72257826e01a94253b8b03f89292d3c
Philip Lages <psilvaelages@gmail.com>
2022-06-30 22:15:47 -0300

    Requirement 5

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,17 +1,21 @@
 const data = require('../data/zoo_data');
 
-// const { price } = data;
+const { prices } = data;
 
 function countEntrants(entrants) {
-//   const child = entrants.filter((entrant) => entrant.age < 18).length;
-//   const adult = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50).length;
-//   const senior = entrants.filter((entrant) => entrant.age >= 50).length;
-//   const obj = { child, adult, senior };
-//   return obj;
+  const child = entrants.filter((entrant) => entrant.age < 18).length;
+  const adult = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50).length;
+  const senior = entrants.filter((entrant) => entrant.age >= 50).length;
+  const obj = { child, adult, senior };
+  return obj;
 }
 
 function calculateEntry(entrants) {
-
+  if (!entrants || Object.keys(entrants).length === 0) return 0;
+  const priceC = countEntrants(entrants).child * prices.child;
+  const priceA = countEntrants(entrants).adult * prices.adult;
+  const priceS = countEntrants(entrants).senior * prices.senior;
+  return priceC + priceA + priceS;
 }
 
 module.exports = { calculateEntry, countEntrants };
