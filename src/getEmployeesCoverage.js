ef960f5c288556e91c1ce1f6b468c1f13eaeb224
Philip Lages <psilvaelages@gmail.com>
2022-07-01 16:15:13 -0300

    Fix requirement 9 lint problems

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,27 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { species, employees } = data;
+
+const getEmployees = () => employees.reduce((acc, curr) => {
+  const getResponsible = species.filter(({ id }) => curr.responsibleFor.includes(id));
+  acc.push({
+    id: curr.id,
+    fullName: `${curr.firstName} ${curr.lastName}`,
+    species: getResponsible.map(({ name }) => name),
+    locations: getResponsible.map(({ location }) => location),
+  });
+  return acc;
+}, []);
+
+function getEmployeesCoverage(employeeCoverage) {
+  if (!employeeCoverage) return getEmployees();
+  const objToArray = Object.entries(employeeCoverage)[0];
+  const name = getEmployees().find((employee) => employee.fullName.includes(objToArray[1]));
+  const id = getEmployees().find((employee) => employee.id.includes(objToArray[1]));
+  return (objToArray[0] === 'name') ? name : id;
+  // if (!name || !id) throw new Error(/^Informações inválidas$/);
 }
+
+// console.log(getEmployeesCoverage({ name: 'Sharonda' }));
 
 module.exports = getEmployeesCoverage;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -2,14 +2,14 @@ const data = require('../data/zoo_data');
 
 const { species, hours } = data;
 
-const getSpecieSched = ((animal) => species.find((specie) => specie.name === animal).availability);
+const getSpecSched = ((animal) => species.find(({ name }) => name === animal).availability);
 
-const getFullSchedule = () => Object.entries(hours).reduce((acc, curr) => {
+const getFull = () => Object.entries(hours).reduce((acc, curr) => {
   if (curr[0] !== 'Monday') {
     acc[curr[0]] = {
       officeHour: `Open from ${curr[1].open}am until ${curr[1].close}pm`,
-      exhibition: species.filter((specie) => specie.availability.includes(curr[0]))
-        .map((specie) => specie.name),
+      exhibition: species.filter(({ availability }) => availability.includes(curr[0]))
+        .map(({ name }) => name),
     };
   }
   acc.Monday = { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
@@ -17,23 +17,11 @@ const getFullSchedule = () => Object.entries(hours).reduce((acc, curr) => {
 }, {});
 
 function getSchedule(scheduleTarget) {
-  const animals = species.map((specie) => specie.name).includes(scheduleTarget);
+  const animals = species.map(({ name }) => name).includes(scheduleTarget);
   const days = Object.keys(hours).includes(scheduleTarget);
-  switch (true) {
-  case !scheduleTarget:
-  case !animals && !days:
-    return getFullSchedule();
-  case animals:
-    return getSpecieSched(scheduleTarget);
-  case days:
-    return { [scheduleTarget]: getFullSchedule()[scheduleTarget] };
-  default:
-    return null;
-  }
+  if (!scheduleTarget) return getFull();
+  if (!animals && !days) return getFull();
+  return (animals) ? getSpecSched(scheduleTarget) : { [scheduleTarget]: getFull()[scheduleTarget] };
 }
-// if (!scheduleTarget) return getFullSchedule();
-// if (!animals && !days) return getFullSchedule();
-// if (animals) return getSpecieSched(scheduleTarget);
-// if (days) return { [scheduleTarget]: getFullSchedule()[scheduleTarget] };
 
 module.exports = getSchedule;
