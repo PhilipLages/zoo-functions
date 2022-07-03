d92ee080ceb0fc9f1220a696615f39ea0b24ee9d
Philip Lages <psilvaelages@gmail.com>
2022-07-02 22:34:39 -0300

    Add requirement 14: 4/7 tests passed

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,34 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+const getSpeciesByLocation = () => species.reduce((acc, curr) => {
+  acc[curr.location] = species.filter(({ location }) => location === curr.location)
+    .map(({ name }) => name);
+  return acc;
+}, {});
+
+const getNames = (specieName) => species.find(({ name }) => name === specieName).residents
+  .map(({ name }) => name);
+
+const getSpeciesNames = () => species.reduce((acc, curr1) => {
+  acc[curr1.location] = species.reduce((acc2, curr2) => {
+    if (curr2.location === curr1.location) {
+      acc2.push({ [curr2.name]: getNames(curr2.name) });
+    }
+    return acc2;
+  }, []);
+  return acc;
+}, {});
+
 function getAnimalMap(options) {
-  // seu código aqui
+  if (!options) return getSpeciesByLocation();
+  const objOptions = Object.keys(options);
+  const includes = (string) => objOptions.includes(string);
+  if (includes('sex') || includes('sorted')) return getSpeciesByLocation();
+  if (includes('includeNames')) return getSpeciesNames();
 }
+
+// console.log(Object.keys({ sex: 'female', sorted: true }));
 
 module.exports = getAnimalMap;
