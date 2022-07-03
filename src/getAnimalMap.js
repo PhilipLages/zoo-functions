5ee24e3a1123dec2b1714b53e422320684cb8c42
Philip Lages <psilvaelages@gmail.com>
2022-07-03 20:17:31 -0300

    Add requirement 14: 7/7 tests passed

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -11,10 +11,38 @@ const getSpeciesByLocation = () => species.reduce((acc, curr) => {
 const getNames = (specieName) => species.find(({ name }) => name === specieName).residents
   .map(({ name }) => name);
 
-const getSpeciesNames = () => species.reduce((acc, curr1) => {
+const getNamesBySex = (specieName, whichSex) => species.find(({ name }) => name === specieName)
+  .residents.reduce((acc, curr) => {
+    if (curr.sex === whichSex) {
+      acc.push(curr.name);
+    }
+    return acc;
+  }, []);
+
+const exist = (options, string) => {
+  const getOptionsKeys = Object.keys(options);
+  const func = getOptionsKeys.includes(string);
+  return func;
+};
+
+const getSpecies = (options) => species.reduce((acc, curr1) => {
   acc[curr1.location] = species.reduce((acc2, curr2) => {
-    if (curr2.location === curr1.location) {
+    if (curr2.location === curr1.location && exist(options, 'sorted')) {
+      acc2.push({ [curr2.name]: getNames(curr2.name).sort() });
+    } else if (curr2.location === curr1.location) {
       acc2.push({ [curr2.name]: getNames(curr2.name) });
+    }
+    return acc2;
+  }, []);
+  return acc;
+}, {});
+
+const getSpeciesBySex = (options) => species.reduce((acc, curr1) => {
+  acc[curr1.location] = species.reduce((acc2, curr2) => {
+    if (curr2.location === curr1.location && exist(options, 'sorted')) {
+      acc2.push({ [curr2.name]: getNamesBySex(curr2.name, options.sex).sort() });
+    } else if (curr2.location === curr1.location) {
+      acc2.push({ [curr2.name]: getNamesBySex(curr2.name, options.sex) });
     }
     return acc2;
   }, []);
@@ -23,12 +51,9 @@ const getSpeciesNames = () => species.reduce((acc, curr1) => {
 
 function getAnimalMap(options) {
   if (!options) return getSpeciesByLocation();
-  const objOptions = Object.keys(options);
-  const includes = (string) => objOptions.includes(string);
-  if (includes('sex') || includes('sorted')) return getSpeciesByLocation();
-  if (includes('includeNames')) return getSpeciesNames();
+  if (!exist(options, 'includeNames')) return getSpeciesByLocation();
+  if (exist(options, 'includeNames') && exist(options, 'sex')) return getSpeciesBySex(options);
+  return getSpecies(options);
 }
-
-// console.log(Object.keys({ sex: 'female', sorted: true }));
 
 module.exports = getAnimalMap;
